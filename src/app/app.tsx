e95612b35986a948b062a30769c7edc83a9eef86
tiagolimarocha <tiago.rocha@radically.digital>
2023-05-11 11:34:43 +0100

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="markdown-editor" />
+    </div>
+  );
+}
+
+export default App;
