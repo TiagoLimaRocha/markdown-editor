1ce63d96cbb715d9f72870f67a164cf1bf2c2a93
tiagolimarocha <tiago.rocha@radically.digital>
2023-05-13 16:07:20 +0100

    Created React app component and added styling

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,53 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
+import React, { FC, useState, useEffect } from 'react';
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
 
-export function App() {
+import { MarkdownAPI } from 'src/api';
+
+export const App: FC = () => {
+  const [markdown, setMarkdown] = useState<string>('');
+
+  const handleMarkdownChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setMarkdown(event.target.value);
+  };
+
+  //  NOTE:  Fetch the previously stored markdown
+  useEffect(() => {
+    MarkdownAPI.get().then((response) => {
+      console.log(response);
+      setMarkdown(response);
+    });
+  }, []);
+
+  //  NOTE:  Upsert the new markdown changes
+  useEffect(() => {
+    if (!!markdown) {
+      MarkdownAPI.upsert(markdown);
+    }
+  }, [markdown]);
+
   return (
-    <div>
-      <NxWelcome title="markdown-editor" />
+    <div className={styles.app}>
+      <textarea
+        className={styles.editor}
+        value={markdown}
+        onChange={handleMarkdownChange}
+        name="editor"
+        id="editor"
+      ></textarea>
+
+      <ReactMarkdown
+        className={styles.reactMarkdown}
+        children={markdown}
+        remarkPlugins={[gfm]}
+      />
     </div>
   );
-}
+};
 
 export default App;
